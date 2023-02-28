9c153bc0517964dc7b7b66331f9fd60722cc5728
VictoriaLind <victoria@lindstar.fi>
2023-02-28 15:47:41 +0200

    Added frontend and requirements

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from my_app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('new_search', views.new_search, name='new_search'),
 ]
